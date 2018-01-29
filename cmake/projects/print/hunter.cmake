f92f2b8233888b9fb51bfc32d9d74902ad97d6d8
Goganych <goga-kulikov@mail.ru>
2018-01-29 03:11:06 +0300

    added print package

---
+++ cmake/projects/print/hunter.cmake
@@ -0,0 +1,27 @@
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    print
+    VERSION
+    "0.1.0.0"
+    URL
+    "https://github.com/Goganych/lab09/archive/v0.1.0.0.tar.gz"
+    SHA1
+    22fa0645d5971cc70707e499bd25f81c275a9170
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+
+hunter_cmake_args(
+    print
+    CMAKE_ARGS
+    BUILD_EXAMPLES=NO
+    BUILD_TESTS=NO
+)
+hunter_cacheable(print)
+hunter_download(PACKAGE_NAME print)
